cb6dc083370bcfc5c69230a73da35685a43589d6
Rob Parker <into.the.parker@gmail.com>
2018-08-19 20:09:39 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,16 +4,16 @@ def countdown(number)
   while number >= 0
     puts "#{number} SECOND(S)!"
     number -= 1
-  end    
+  end
 end
 
 def countdown_with_sleep(number)
-  while number >= 0 
+  while number >= 0
     puts "#{number} SECOND(S)!"
     number -= 1
     sleep(1.sec)
   end
-  end   
+  end
 
 
 ## Instructions
